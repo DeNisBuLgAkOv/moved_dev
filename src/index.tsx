75ee0a30d0a7eaed6d1e92354e6e9eb13646130d
DeNisBuLgAkOv <bulgackoff.denis@yandex.ru>
2023-09-27 22:03:55 +0300

    first

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import {render} from "react-dom";
+import cls from './index.module.scss'
+
+
+
+render(
+  <div className={cls.nn}>asdsdvdsfdffdsfzxczxcxzsd</div>,
+  document.getElementById('root')
+)
---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import webpack from "webpack";
+import {BuildWebpackconfig} from "./config/build/buildWebpackconfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path";
+
+
+
+
+export default (env:BuildEnv) =>{
+  console.log(env)
+
+  const paths:BuildPaths ={
+    entry:path.resolve(__dirname,'src','index.tsx'),
+    html:path.resolve(__dirname, 'public', 'index.html'),
+    build:path.resolve(__dirname, 'build'),
+  }
+
+  const mode = env.mode || "development"
+  const isDev = mode === "development"
+  const PORT = env.port || 3000
+
+  console.log(env.mode)
+  const config:webpack.Configuration = BuildWebpackconfig({
+    mode:mode,
+    paths,
+    isDev,
+    port:PORT
+  })
+
+  return config
+}
