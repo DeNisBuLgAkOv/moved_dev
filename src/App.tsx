f3cf9e426da9e41d5dc2640b438dfb39045fb375
DeNisBuLgAkOv <bulgackoff.denis@yandex.ru>
2023-09-29 22:54:47 +0300

    Added new themes and organized styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, {Suspense} from 'react'
+import React, {Suspense, useContext, useState} from 'react'
 import {Routes, Route, Link} from 'react-router-dom';
 import './styles/index.scss'
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+
+const {theme,toggleTheme} =useTheme()
   return (
-    <div className={'app'}>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}> Toggle</button>
       <Link to={'/'}>
         главная
       </Link>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import {render} from "react-dom";
 import cls from './index.module.scss'
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 
 render(
   <BrowserRouter>
+    <ThemeProvider>
     <App/>
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
